ef1b3310dcd082e2eabd6fee109a6305db72db44
britishpandachick <sbartley1080@gmail.com>
2023-01-18 11:01:20 -0500

    Update WordPress version, themes, and plugins. Remove twenty twenty two theme

--- wp-content/plugins/cookie-notice/js/admin-notice.js
+++ wp-content/plugins/cookie-notice/js/admin-notice.js
@@ -2,19 +2,18 @@
 
 	// ready event
 	$( function() {
-		// Save dismiss state // .is-dismissible
+		// save dismiss state // .is-dismissible
 		$( '.cn-notice' ).on( 'click', '.notice-dismiss, .cn-notice-dismiss', function( e ) {
-	
 			var notice_action = 'dismiss';
 			var param = '';
-			
+
 			if ( $( e.currentTarget ).hasClass( 'cn-approve' ) )
 				notice_action = 'approve';
 			else if ( $( e.currentTarget ).hasClass( 'cn-delay' ) )
 				notice_action = 'delay';
 			else if ( $( e.delegateTarget ).hasClass( 'cn-threshold' ) ) {
 				notice_action = 'threshold';
-				
+
 				var delay = $( e.delegateTarget ).find( '.cn-notice-text' ).data( 'delay' );
 
 				param = parseInt( delay );
@@ -28,7 +27,8 @@
 					action: 'cn_dismiss_notice',
 					notice_action: notice_action,
 					nonce: cnArgsNotice.nonce,
-					param: param
+					param: param,
+					cn_network: cnArgsNotice.network
 				}
 			} );
 
