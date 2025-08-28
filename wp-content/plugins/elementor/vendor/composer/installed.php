98cc4456c8830e8f56c80db5b44dc5f2e06dc3ba
britishpandachick <sbartley1080@gmail.com>
2025-08-28 14:32:08 -0400

    Update plugins and themes. Remove twenty twentyfive theme

--- wp-content/plugins/elementor/vendor/composer/installed.php
+++ wp-content/plugins/elementor/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'elementor/elementor',
-        'pretty_version' => '3.29.x-dev',
-        'version' => '3.29.9999999.9999999-dev',
-        'reference' => '686b904ebe5b27a008de4b9150e0534f2fcbafbb',
+        'pretty_version' => '3.31.x-dev',
+        'version' => '3.31.9999999.9999999-dev',
+        'reference' => '0ed0070e8f0d22fc45a283fb86c35f0f6c6ede0f',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'elementor/elementor' => array(
-            'pretty_version' => '3.29.x-dev',
-            'version' => '3.29.9999999.9999999-dev',
-            'reference' => '686b904ebe5b27a008de4b9150e0534f2fcbafbb',
+            'pretty_version' => '3.31.x-dev',
+            'version' => '3.31.9999999.9999999-dev',
+            'reference' => '0ed0070e8f0d22fc45a283fb86c35f0f6c6ede0f',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
